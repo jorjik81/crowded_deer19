4ccc34c71dbc3a01730dfb629a8012fa0119fe9e
Gio <jorjik81@gmail.com>
2023-12-01 20:31:28 +0400

    Manifest json data injected

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -23,12 +23,32 @@ module.exports = () => {
       template: './index.html',
       title: 'Text Editor'
     }),
-    
+
     // Injects our custom service worker
     new InjectManifest({
       swSrc: './src-sw.js',
       swDest: 'src-sw.js',
-    }),  
+    }), 
+    
+    // Creates a manifest.json file.
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: true,
+      name: 'Text Editor',
+      short_name: 'TE',
+      description: 'Take notes!',
+      background_color: '#225ca3',
+      theme_color: '#225ca3',
+      start_url: './',
+      publicPath: './',
+      icons: [
+        {
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
+      ],
+    }),
     ],
 
     module: {
